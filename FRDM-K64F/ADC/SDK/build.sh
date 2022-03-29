f6d604b60bcb5055f122ae5f8be723504d653bb6
dataisland <dataisland@outlook.com>
2022-03-29 16:06:05 +0800

    Ignore log files

--- FRDM-K64F/ADC/SDK/build.sh
+++ FRDM-K64F/ADC/SDK/build.sh
@@ -4,4 +4,4 @@ if [ -f "Makefile" ];then rm -f Makefile; fi
 if [ -f "cmake_install.cmake" ];then rm -f cmake_install.cmake; fi
 if [ -f "CMakeCache.txt" ];then rm -f CMakeCache.txt; fi
 cmake -DCMAKE_TOOLCHAIN_FILE="armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=debug  .
-make -j 2>&1 | tee build_log.txt
+make -j 2>&1 | tee debug/build_log.txt
