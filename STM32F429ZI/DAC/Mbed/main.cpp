c0ffc54aad39a3538c9ef0108bc8dc585afac28f
dataisland <dataisland@outlook.com>
2022-04-02 00:13:53 +0800

    Create main.cpp

---
+++ STM32F429ZI/DAC/Mbed/main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+ 
+// DAC 20khz
+#define DAC_Frequency 20000
+ 
+AnalogOut dac(PA_4);
+
+
+int main()
+{
+    dac = sin(1 * 3.14f * 2 / (1000000 / 262)) / 2.0f + 0.5f;
+    dac = sin(2 * 3.14f * 2 / (1000000 / 262)) / 2.0f + 0.5f;
+    dac = sin(3 * 3.14f * 2 / (1000000 / 262)) / 2.0f + 0.5f;
+ 
+    while(1) {
+    }
+}
+ 
