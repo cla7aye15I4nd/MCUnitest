581f8c7276b60075f61b039d5718cc1b839dab77
dataisland <dataisland@outlook.com>
2022-04-05 15:35:37 +0800

    Add some SAM3X8E Test

--- test/test.py
+++ test/test.py
@@ -8,11 +8,8 @@ from qiling.extensions.mcu.atmel import sam3x8e
 
 class SAM3X8ETest(unittest.TestCase):
     def qiling_common_setup(self, path):
-        return Qiling([path], archtype="cortex_m", ostype="mcu", env=sam3x8e)
-
-    def test_uart_arduino(self):
-        ql = self.qiling_common_setup('../target/official/SAM3X8E_UART_Arduino.elf')
-
+        ql = Qiling([path], archtype="cortex_m", ostype="mcu", env=sam3x8e)
+        
         ql.hw.create('wdt')
         ql.hw.create('efc0')
         ql.hw.create('efc1')
@@ -23,37 +20,78 @@ class SAM3X8ETest(unittest.TestCase):
         ql.hw.create('pioc')
         ql.hw.create('piod')
         ql.hw.create('adc')
-        ql.hw.create('uart').watch()
+        ql.hw.create('uart')
         ql.hw.create('pdc_uart')
 
+        return ql
+
+    def test_uart_arduino(self):
+        ql = self.qiling_common_setup('../target/official/SAM3X8E_UART_Arduino.elf')
+        ql.hw.uart.watch()
+        
         ql.hw.uart.send(b'BCDE')
         ql.run(count=100000)
 
         self.assertTrue(ql.hw.uart.recv() == b'ABCDE')
-
         del ql
 
     def test_uart_riot(self):
         ql = self.qiling_common_setup('../target/other/SAM3X8E_UART_RIOT.elf')
-
-        ql.hw.create('wdt')
-        ql.hw.create('efc0')
-        ql.hw.create('efc1')
-        ql.hw.create('pmc')
-        ql.hw.create('uotghs')
-        ql.hw.create('pioa')
-        ql.hw.create('piob')
-        ql.hw.create('pioc')
-        ql.hw.create('piod')
-        ql.hw.create('adc')
-        ql.hw.create('uart').watch()
-        ql.hw.create('pdc_uart')
+        ql.hw.uart.watch()
 
         ql.hw.uart.send(b'BCDE')
         ql.run(count=100000)
 
         self.assertTrue(ql.hw.uart.recv() == b'main(): This is RIOT! (Version: 2022.04-devel-1050-gfca56)\nABCDE')
+        del ql
 
+    def test_adc_arduino(self):
+        ql = self.qiling_common_setup('../target/official/SAM3X8E_ADC_Arduino.elf')
+        ql.hw.adc.watch()
+
+        ql.run(count=100000)
+        self.assertTrue(ql.hw.uart.recv().startswith(b'5.00\r\n5.00\r\n'))
+        del ql
+
+    def test_adc_riot(self):
+        ql = self.qiling_common_setup('../target/other/SAM3X8E_ADC_RIOT.elf')
+        ql.hw.adc.watch()
+
+        ql.run(count=100000)
+        self.assertTrue(ql.hw.uart.recv().startswith(b'main(): This is RIOT! (Version: 2022.04-devel-1050-gfca56)\nadc = 4095\n'))
+        del ql
+
+    def test_dac_arduino(self):
+        ql = self.qiling_common_setup('../target/official/SAM3X8E_DAC_Arduino.elf')
+        ql.hw.create('dac')        
+
+        flags = [False, False]
+        def hook_set():
+            print("LED on")
+            flags[0] = True
+        
+        def hook_rst():
+            print("LED off")
+            flags[1] = True
+            
+        ql.hw.piob.hook_set  (27, hook_set)
+        ql.hw.piob.hook_reset(27, hook_rst)
+
+        ql.hw.systick.ratio = 0xfff
+        ql.run(count=50000)
+
+        self.assertTrue(flags[0] and flags[1])
+        
+        del ql
+
+    def test_dac_riot(self):
+        ql = self.qiling_common_setup('../target/other/SAM3X8E_DAC_RIOT.elf')
+        ql.hw.create('dac').watch()
+
+        ql.hw.systick.ratio = 0xfff
+        ql.run(count=200000)
+        self.assertTrue(ql.hw.uart.recv() == b'main(): This is RIOT! (Version: 2022.04-devel-1050-gfca56)\nok\n')
+        
         del ql
 
 if __name__ == '__main__':
