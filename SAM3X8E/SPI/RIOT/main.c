5287a995f99c04f7b61845e30a0bbccf14206540
dataisland <dataisland@outlook.com>
2022-04-08 21:30:50 +0800

    Fix SAM3X8E example

--- SAM3X8E/SPI/RIOT/main.c
+++ SAM3X8E/SPI/RIOT/main.c
@@ -4,13 +4,16 @@
 #include <periph/spi.h>
 #include <board.h>
 
+uint8_t data[] = "a";
 int main(void)
 {    
     spi_init(SPI_DEV(0));
     spi_acquire(SPI_DEV(0), SPI_CS_UNDEF, SPI_MODE_0, SPI_CLK_1MHZ);
 
-    uint8_t data = 'a';   
-    data = spi_transfer_byte(SPI_DEV(0), SPI_CS_UNDEF, false, data);
+    for (int i = 0; i < 10; ++i) {
+        data[0] = spi_transfer_byte(SPI_DEV(0), SPI_CS_UNDEF, false, data[0]);
+        printf("%c", data[0]);
+    }
 
     spi_release(SPI_DEV(0));
    
