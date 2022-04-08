057a533f2fcdc87b165c5b254b6000c43928cd42
dataisland <dataisland@outlook.com>
2022-04-08 21:52:57 +0800

    Fix Spi Example

--- SAM3X8E/SPI/Arduino/SpiReadWrite/SpiReadWrite.ino
+++ SAM3X8E/SPI/Arduino/SpiReadWrite/SpiReadWrite.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 
 void setup() {
+    Serial.begin(115200);
     // initialize the bus for the device on pin 4
     SPI.begin(4);
 
@@ -14,23 +15,15 @@ void setup() {
     SPI.setClockDivider(10, 84);
 
     pinMode(LED_BUILTIN, OUTPUT);
+    
+    byte rep = 0xee;
+
+    for (int i = 0; i < 10; ++i) {
+        rep = SPI.transfer(10, rep, SPI_CONTINUE);
+        Serial.write(rep);
+    }
 }
 
 void loop() {
-    //transfer 0x0F to the device on pin 10, keep the chip selected
-    SPI.transfer(10, 0xF0, SPI_CONTINUE);
-
-    //transfer 0x00 to the device on pin 10, keep the chip selected
-    SPI.transfer(10, 0xac, SPI_CONTINUE);
-
-    // //transfer 0x00 to the device on pin 10, store byte received in response1, keep the chip selected
-    byte response1 = SPI.transfer(10, 0xdb, SPI_CONTINUE);
-
-    // //transfer 0x00 to the device on pin 10, store byte received in response2, deselect the chip
-    byte response2 = SPI.transfer(10, 0xfe);
-
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+    
 }
