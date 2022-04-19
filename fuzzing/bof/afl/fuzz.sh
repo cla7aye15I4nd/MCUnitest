595c96509fe8b38e54dae2beea11ea05fa637709
dataisland <dataisland@outlook.com>
2022-04-19 20:01:08 +0800

    Update afl env

--- fuzzing/bof/afl/fuzz.sh
+++ fuzzing/bof/afl/fuzz.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-set -e
-if [[ ! -d ./AFLplusplus ]]; then
-  git clone https://github.com/AFLplusplus/AFLplusplus.git
-  cd AFLplusplus
-  make
-  cd ./unicorn_mode
-  ./build_unicorn_support.sh
-  cd ../../
-fi
-AFL_AUTORESUME=1 AFL_PATH="$(realpath ./AFLplusplus)" PATH="$AFL_PATH:$PATH" afl-fuzz -i inputs -o outputs -U -- python3 ./fuzz.py @@
+AFL_AUTORESUME=1 afl-fuzz -i inputs -o outputs -U -- python3 ./fuzz.py @@
