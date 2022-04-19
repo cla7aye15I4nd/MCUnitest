a1e92af125ed4183e86187ab82c66afd3bc7b086
dataisland <dataisland@outlook.com>
2022-04-19 20:26:00 +0800

    Optimize hack script

--- fuzzing/bof/afl/hack.py
+++ fuzzing/bof/afl/hack.py
@@ -7,15 +7,12 @@ import sys, os
 sys.path.append("../../../../qiling")
 
 from qiling.core import Qiling
+from qiling.const import QL_VERBOSE
 from qiling.extensions.mcu.stm32f4 import stm32f429
 
-ql = Qiling(["../build/bof.elf"], archtype="cortex_m", env=stm32f429, ostype='mcu')
+ql = Qiling(["../build/bof.elf"], archtype="cortex_m", env=stm32f429, ostype='mcu', verbose=QL_VERBOSE.DISABLED)
 
-ql.hw.create('pwr')
 ql.hw.create('rcc')
-ql.hw.create('flash interface')
-ql.hw.create('gpioa')
-ql.hw.create('gpioc')
 ql.hw.create('usart2')
 ql.hw.create('usart3')
 
@@ -23,7 +20,7 @@ snapshot = ql.save(hw=True)
 
 ql.restore(snapshot)
 ql.hw.usart3.send(b'abc\n')
-ql.run(count=100000)
+ql.run(count=20000)
 
 print('[Normal]')
 print('USART2 Output:', ql.hw.usart2.recv())
@@ -31,7 +28,7 @@ print('USART3 Output:', ql.hw.usart3.recv())
 
 ql.restore(snapshot)
 ql.hw.usart3.send(b'aaaaaaaaaaaaaaaaaaaa\x89\x05\n')
-ql.run(count=100000)
+ql.run(count=20000)
 
 print('[Overflow]')
 print('USART2 Output:', ql.hw.usart2.recv())
